d994db8a729d342de084cf75ee013fa64a8fea14
Stéphane Lepin <stephane.lepin@gmail.com>
2020-08-08 03:58:29 +0200

    source(aes67): props setup

--- src/aes67-source.cpp
+++ src/aes67-source.cpp
@@ -16,17 +16,30 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
+#include <obs.h>
 #include <obs-module.h>
 #include <util/platform.h>
 #include <util/threading.h>
 
 #include "plugin-macros.generated.h"
 
+#define P_MULTICAST_GROUP "multicast_group"
+#define P_SAMPLE_RATE "sample_rate"
+#define P_SPEAKER_LAYOUT "speaker_layout"
+#define P_SAMPLE_FORMAT "sample_format"
+
+#define O_SAMPLE_FORMAT_L16 1
+#define O_SAMPLE_FORMAT_L24 2
+
 struct aes67_source
 {
 	obs_source_t* source;
 	bool running;
 	pthread_t receiver_thread;
+	const char* multicast_group;
+	int sample_rate;
+	enum speaker_layout speakers;
+	int sample_format;
 	os_performance_token_t* perf_token;
 };
 
@@ -43,13 +56,43 @@ obs_properties_t* aes67_source_getproperties(void* data)
 	obs_properties_t* props = obs_properties_create();
 	obs_properties_set_flags(props, OBS_PROPERTIES_DEFER_UPDATE);
 
+	obs_properties_add_text(props,
+		P_MULTICAST_GROUP, "Multicast Group",
+		OBS_TEXT_DEFAULT
+	);
+
+	obs_property_t* prop;
+	prop = obs_properties_add_list(props,
+		P_SAMPLE_RATE, "Sample Rate",
+		OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT
+	);
+	obs_property_list_add_int(prop, "48 kHz", 48000);
+	obs_property_list_add_int(prop, "96 kHz", 96000);
+
+	prop = obs_properties_add_list(props,
+		P_SPEAKER_LAYOUT, "Channels",
+		OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT
+	);
+	obs_property_list_add_int(prop, "Mono", SPEAKERS_MONO);
+	obs_property_list_add_int(prop, "Stereo", SPEAKERS_STEREO);
+	obs_property_list_add_int(prop, "7.1", SPEAKERS_7POINT1);
+
+	prop = obs_properties_add_list(props,
+		P_SAMPLE_FORMAT, "Sample Format",
+		OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT
+	);
+	obs_property_list_add_int(prop, "L16", O_SAMPLE_FORMAT_L16);
+	obs_property_list_add_int(prop, "L24", O_SAMPLE_FORMAT_L24);
+
 	return props;
 }
 
 void aes67_source_getdefaults(obs_data_t* settings)
 {
-	// TODO
-	UNUSED_PARAMETER(settings);
+	obs_data_set_default_string(settings, P_MULTICAST_GROUP, "");
+	obs_data_set_default_int(settings, P_SAMPLE_RATE, 48000);
+	obs_data_set_default_int(settings, P_SPEAKER_LAYOUT, (int)SPEAKERS_STEREO);
+	obs_data_set_default_int(settings, P_SAMPLE_FORMAT, O_SAMPLE_FORMAT_L24);
 }
 
 void* aes67_receiver_thread(void* data)
@@ -85,6 +128,11 @@ void aes67_source_update(void* data, obs_data_t* settings)
 	}
 	s->running = false;
 
+	s->multicast_group = obs_data_get_string(settings, P_MULTICAST_GROUP);
+	s->sample_rate = obs_data_get_int(settings, P_SAMPLE_RATE);
+	s->speakers = (enum speaker_layout)obs_data_get_int(settings, P_SPEAKER_LAYOUT);
+	s->sample_format = obs_data_get_int(settings, P_SAMPLE_FORMAT);
+
 	// TODO setup receiver
 
 	s->running = true;
@@ -95,7 +143,6 @@ void* aes67_source_create(obs_data_t* settings, obs_source_t* source)
 {
 	auto s = (struct aes67_source*)bzalloc(sizeof(struct aes67_source));
 	s->source = source;
-	s->running = false;
 	s->perf_token = NULL;
 	aes67_source_update(s, settings);
 	return s;
