74678ee09f2cc0790464d7fa403d5edd64bf4b6e
Stéphane Lepin <stephane.lepin@gmail.com>
2020-08-08 17:44:17 +0200

    status-label: add config option

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -19,20 +19,28 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 #include <obs-frontend-api.h>
+#include <util/config-file.h>
 
 #include "plugin-macros.generated.h"
 
 #include "utils.h"
 #include "status-label.h"
 
+#define CONFIG_SECTION "obs-tools"
+
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 void on_finished_loading(void* param);
 
+bool statusLabelEnabled = false;
+
 bool obs_module_load(void)
 {
 	blog(LOG_INFO, "version %s", PLUGIN_VERSION);
+
+	config_t* globalConfig = obs_frontend_get_global_config();
+	statusLabelEnabled = config_get_bool(globalConfig, CONFIG_SECTION, "StatusLabelEnabled");
 
 	// defer GUI setup when frontend is ready
 	utils::register_frontend_event_once(
@@ -46,7 +54,9 @@ bool obs_module_load(void)
 void on_finished_loading(void* param)
 {
 	// call setup procedure(s)
-	status_label::setup();
+	if (statusLabelEnabled) {
+		status_label::setup();
+	}
 
 	UNUSED_PARAMETER(param);
 }
@@ -54,7 +64,9 @@ void on_finished_loading(void* param)
 void obs_module_unload()
 {
 	// call teardown procedure(s)
-	status_label::teardown();
+	if (statusLabelEnabled) {
+		status_label::teardown();
+	}
 
 	blog(LOG_INFO, "plugin unloaded");
 }
